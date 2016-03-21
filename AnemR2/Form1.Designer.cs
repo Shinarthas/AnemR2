018686b495fcb2500e21c2b31621d7e9f3b19500
Shinarthas <tttaaabbb@gmail.com>
2016-03-22 01:39:03 +0200

    1
    asas

--- AnemR2/Form1.Designer.cs
+++ AnemR2/Form1.Designer.cs
@@ -38,6 +38,14 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -46,6 +54,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "14,5";
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
@@ -54,6 +63,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "4";
             this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox3
@@ -62,6 +72,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "12,19";
             this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label1
@@ -97,7 +108,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(186, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "execute";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -116,6 +127,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "58";
             this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label5
@@ -127,11 +139,85 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "age";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(246, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "P";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 99);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(0, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(246, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "sgot";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(246, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "chol";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(246, 107);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "trig";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(246, 124);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "platent";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(382, 261);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
@@ -143,7 +229,8 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Anemia";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +248,13 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- AnemR2/Form1.cs
+++ AnemR2/Form1.cs
@@ -16,7 +16,7 @@ namespace AnemR2
         {
             InitializeComponent();
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
             string t1 = (textBox1.Text);
@@ -29,7 +29,14 @@ namespace AnemR2
                 return;
             }
             label4.Text="status: "+calcStatus().ToString();
+            label6.Text = "P: " + calcP().ToString();
+            label8.Text = "sgot: " + Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox1.Text)/6;
+            label9.Text = "chol: " + Convert.ToDouble(textBox2.Text) + Convert.ToDouble(textBox3.Text) / 2;
+            label10.Text = "trig: " + Convert.ToDouble(textBox4.Text) * Convert.ToDouble(textBox3.Text) / 2;
+            label11.Text = "platent: " + Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text) + Convert.ToDouble(textBox4.Text);
+            pictureBox1.Refresh();
         }
+        
 
         public int calcStatus()
         {
@@ -45,7 +52,20 @@ namespace AnemR2
             double res = t1*c1 + t2*c2 + t3*c3 + t4*c4 + c5;
             return res > 0 ? 1 : 0;
         }
-
+        public double calcP()
+        {
+            double t1 = Convert.ToDouble(textBox1.Text);
+            double t2 = Convert.ToDouble(textBox2.Text);
+            double t3 = Convert.ToDouble(textBox3.Text);
+            double t4 = Convert.ToDouble(textBox4.Text);
+            double c1 = 0.317;
+            double c2 = 0.416;
+            double c3 = 0.676;
+            double c4 = 0.047;
+            double c5 = -12.165;
+            double res = t1 * c1 + t2 * c2 + t3 * c3 + t4 * c4 + c5;
+            return res;
+        }
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar >= '0' && e.KeyChar <= '9')
@@ -72,6 +92,28 @@ namespace AnemR2
                 return;
             }
             e.Handled = true;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p1=new Pen(Color.Black,2);
+            Pen p2 = new Pen(Color.DarkRed, 2);
+            Pen p3 = new Pen(Color.DarkMagenta, 2);
+            Pen p4 = new Pen(Color.DarkOrange, 2);
+            e.Graphics.DrawLine(p1, 0, Convert.ToSingle(textBox1.Text), 400, Convert.ToSingle(textBox1.Text) * Convert.ToSingle(textBox1.Text) / 6);
+            e.Graphics.DrawLine(p2, 0, Convert.ToSingle(textBox2.Text), 400, Convert.ToSingle(textBox2.Text) + Convert.ToSingle(textBox3.Text) / 2);
+            e.Graphics.DrawLine(p3, 0, Convert.ToSingle(textBox3.Text), 400, Convert.ToSingle(textBox4.Text) * Convert.ToSingle(textBox3.Text) /10);
+            e.Graphics.DrawLine(p4, 0, Convert.ToSingle(textBox4.Text), 400, Convert.ToSingle(textBox1.Text) * Convert.ToSingle(textBox3.Text)/10 + Convert.ToSingle(textBox4.Text)-60);
         }
     }
 }
